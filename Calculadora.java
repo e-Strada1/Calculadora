5afb7c27e4e428686210669502b2c20d42f46d87
e-Strada1 <estradariquelme2@gmail.com>
2024-11-21 20:02:31 +0100

    Update Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -20,6 +20,13 @@ public class Calculadora{
                 resultado = n1+n2;
                 System.out.println("El resultado de la suma es: "+resultado);
             }
+            if(opc==2){System.out.println("Introduce un numero");
+            n1 = in.nextInt();
+            System.out.println("Introduce otro numero ");
+            n2 = in.nextInt();
+            resultado = n1-n2;
+            System.out.println("El resultado de la resta es: "+resultado);
+            }
             if(opc==5){
                 System.out.println("Adios!!");
             }
