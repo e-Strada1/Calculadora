2c62514e86e948ee168b3b028b8e5db8e930f6e9
e-Strada1 <estradariquelme2@gmail.com>
2024-11-21 20:01:04 +0100

    Create Calculadora.java

---
+++ Calculadora.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Calculadora{
+
+    public static void main(String args[]){
+
+        Scanner in = new Scanner(System.in);
+        int opc=0;
+        int n1;
+        int n2;
+        int resultado;
+        while(opc !=5){
+            System.out.println("1.Suma de dos numeros.\n2.Resta de numeros.\n3.Multiplicacion de numeros.\n4.División de numeros.\n5.Salir");;
+            opc = in.nextInt();
+            if(opc==1){
+                System.out.println("Introduce un numero");
+                n1 = in.nextInt();
+                System.out.println("Introduce otro numero ");
+                n2 = in.nextInt();
+                resultado = n1+n2;
+                System.out.println("El resultado de la suma es: "+resultado);
+            }
+            if(opc==5){
+                System.out.println("Adios!!");
+            }
+
+        }
+
+
+
+        in.close();
+    }
+}
