3a50473839665591674674e54aa1bb4ef7fbb109
e-Strada1 <estradariquelme2@gmail.com>
2024-12-19 12:01:46 +0100

    Añadida multiplicación

--- Calculadora.java
+++ Calculadora.java
@@ -4,9 +4,18 @@ public class Calculadora{
     public static void main(String args[]){
 
         double num1 = 10.5;
-        char operador = '+';
+        char operador = '*';
         double num2 = 3.2;
-        
+        double resultado = 0;
+        switch (operador) {
+            case '*':
+            resultado = num1 * num2;
+            break;
+        default:
+            System.out.println("Operador inválido");
+        return;
+        }
+        System.out.println("Resultado: " + resultado);
 
     }
 }
