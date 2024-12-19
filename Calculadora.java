2b44abff071ecc2652ebc8cc51db51ef5c6e86cc
e-Strada1 <estradariquelme2@gmail.com>
2024-12-19 11:56:51 +0100

    Creado de main y operadores

--- Calculadora.java
+++ Calculadora.java
@@ -6,16 +6,7 @@ public class Calculadora{
         double num1 = 10.5;
         char operador = '+';
         double num2 = 3.2;
-        double resultado = 0;
-        switch (operador) {
-            case '+':
-                resultado = num1 + num2;
-                break;
-            default:
-                System.out.println("Operador inválido");
-            return;
-            }
-        System.out.println("Resultado: " + resultado);
+        
 
     }
 }
