a7768bbce76c81e45369d2658feaf32a80868a16
e-Strada1 <estradariquelme2@gmail.com>
2024-12-19 12:04:57 +0100

    Añadida división

--- Calculadora.java
+++ Calculadora.java
@@ -6,7 +6,21 @@ public class Calculadora{
         double num1 = 10.5;
         char operador = '+';
         double num2 = 3.2;
-        
+        double resultado = 0;
+        switch (operador) {
+            case '/':
+                if (num2 == 0) {
+                    System.out.println("Error: División entre cero");
+                } else {
+                    resultado = num1 / num2;
+                }
+                break;
+            default:
+                System.out.println("Operador inválido");
+                return;
+        }
+        System.out.println("Resultado: " + resultado);
+
 
     }
 }
